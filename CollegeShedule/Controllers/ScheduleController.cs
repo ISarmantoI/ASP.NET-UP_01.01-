f3c829ad415c3d5d58bf8331f9e57676ebb3bad9
Sarmanto <fleerj2007@gmail.com>
2026-01-13 15:29:22 +0700

    add schedule API controller

---
+++ CollegeShedule/Controllers/ScheduleController.cs
@@ -0,0 +1,24 @@
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+
+        public ScheduleController(IScheduleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start, end);
+            return Ok(result);
+        }
+    }
+}
